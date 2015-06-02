c2d9a8f2bf86b7e4128128997586e2197cffa7c4
Frank Bergmann <frank.bergmann@project-open.com>
2015-06-02 15:27:27 +0000

    - Cost Centers:   - added tree_sortkey   - CCs are still sorted by cost_center_code in the admin screen,     but this may be replaced once the update has proven to work

--- sql/postgresql/intranet-cost-create.sql
+++ sql/postgresql/intranet-cost-create.sql
@@ -118,12 +118,91 @@ create table im_cost_centers (
 				references users,
 	description		text,
 	note			text,
+
+	tree_sortkey		varbit,
+	max_child_sortkey	varbit,
+
 		-- don't allow two cost centers under the same parent
 		constraint im_cost_centers_un
 		unique(cost_center_name, parent_id)
 );
 create index im_cost_centers_parent_id_idx on im_cost_centers(parent_id);
 create index im_cost_centers_manager_id_idx on im_cost_centers(manager_id);
+
+-- Speed up child-sortkey queries
+create index im_cost_centers_treesort_idx on im_cost_centers(tree_sortkey);
+
+
+
+create or replace function im_cost_centers_insert_tr ()
+returns trigger as $$
+DECLARE
+	v_max_child_sortkey		varbit;
+	v_parent_sortkey		varbit;
+BEGIN
+	IF new.parent_id is null THEN
+		new.tree_sortkey := int_to_tree_key(new.cost_center_id);
+	ELSE
+		SELECT	tree_sortkey, tree_increment_key(max_child_sortkey)
+		INTO	v_parent_sortkey, v_max_child_sortkey
+		FROM	im_cost_centers
+		WHERE	cost_center_id = new.parent_id
+		for update;
+
+		update im_cost_centers
+		set max_child_sortkey = v_max_child_sortkey
+		where cost_center_id = new.parent_id;
+
+		new.tree_sortkey := v_parent_sortkey || v_max_child_sortkey;
+	END IF;
+	new.max_child_sortkey := null;
+	RETURN new;
+END;$$ language 'plpgsql';
+
+create or replace function im_cost_centers_update_tr () 
+returns trigger as $$
+DECLARE
+	v_parent_sk		varbit default null;
+	v_max_child_sortkey	varbit;
+	v_old_parent_length	integer;
+BEGIN
+	IF new.cost_center_id = old.cost_center_id and ((new.parent_id = old.parent_id) or (new.parent_id is null and old.parent_id is null)) THEN 
+		return new; 
+	END IF;
+
+	v_old_parent_length := length(new.tree_sortkey) + 1;
+	IF new.parent_id is null THEN
+		v_parent_sk := int_to_tree_key(new.cost_center_id);
+	ELSE
+		SELECT	tree_sortkey, tree_increment_key(max_child_sortkey)
+		INTO v_parent_sk, v_max_child_sortkey
+		FROM im_cost_centers
+		WHERE cost_center_id = new.parent_id
+		FOR UPDATE;
+
+		UPDATE im_cost_centers
+		SET max_child_sortkey = v_max_child_sortkey
+		WHERE cost_center_id = new.parent_id;
+
+		v_parent_sk := v_parent_sk || v_max_child_sortkey;
+	END IF;
+
+	UPDATE im_cost_centers
+	SET tree_sortkey = v_parent_sk || substring(tree_sortkey, v_old_parent_length)
+	WHERE tree_sortkey between new.tree_sortkey and tree_right(new.tree_sortkey);
+
+	RETURN new;
+END;$$ language 'plpgsql';
+
+
+create trigger im_cost_center_insert_tr
+before insert on im_cost_centers
+for each row execute procedure im_cost_center_insert_tr();
+
+create trigger im_cost_centers_update_tr after update
+on im_cost_centers for each row
+execute procedure im_cost_centers_update_tr ();
+
 
 
 
